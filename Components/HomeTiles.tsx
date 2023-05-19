9a3e9f342b2cb3b97dadec8ceee6e4538466caf6
Filip Gurak <filip.gurak999@gmail.com>
2023-05-19 10:19:54 +0200

    speakers tile

--- Components/HomeTiles.tsx
+++ Components/HomeTiles.tsx
@@ -5,7 +5,7 @@ import SecondaryBtn from './SecondaryBtn';
 
 function HomeTiles() {
   return (
-    <section className='px-6 md:px-16 lg:px-32 2xl:px-[20vw] flex flex-col space-y-12 mt-24'>
+    <section className='px-6 md:px-16 lg:px-32 2xl:px-[20vw] flex flex-col space-y-16 mt-16'>
       <div className="flex flex-col items-center text-center md:text-left pt-16 pb-4 md:pt-0 md:flex-row lg:items-end px-12 lg:px-16 xl:px-24 2xl:px-32 bg-orange w-full min-h-[35rem] rounded-md bg-[url('/images/home/desktop/pattern-circles.svg')] bg-contain md:bg-cover bg-no-repeat md:bg-[-16vw_-12vw]">
         <Image
           src='/images/home/desktop/image-speaker-zx9.png'
@@ -31,10 +31,37 @@ function HomeTiles() {
           alt='speaker image'
           width={1920}
           height={1080}
-          className='rounded-md'
+          className='rounded-md hidden md:block'
+        />
+        <Image
+          src='/images/home/tablet/image-speaker-zx7.jpg'
+          alt='speaker image'
+          width={1920}
+          height={1080}
+          className='rounded-md hidden sm:block md:hidden'
+        />
+        <Image
+          src='/images/home/mobile/image-speaker-zx7.jpg'
+          alt='speaker image'
+          width={1920}
+          height={1080}
+          className='rounded-md sm:hidden'
         />
         <div className='absolute top-1/2 left-[10%] -translate-y-1/2 space-y-3'>
           <h2 className='text-3xl font-semibold uppercase'>zx7 speakers</h2>
+          <SecondaryBtn outline>see product</SecondaryBtn>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+        <Image
+          src='/images/home/desktop/image-earphones-yx1.jpg'
+          alt='earphones image'
+          width={1920}
+          height={1080}
+          className='rounded-md'
+        />
+        <div className='py-16 text-center md:text-left md:py-0 flex flex-col px-[15%] justify-center space-y-3 bg-light-gray w-full h-full rounded-md'>
+          <h2 className='text-3xl font-semibold uppercase'>yx1 earphones</h2>
           <SecondaryBtn outline>see product</SecondaryBtn>
         </div>
       </div>
