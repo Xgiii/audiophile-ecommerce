79bf56fc319fa0bbb10d131b411a4b6eb455b234
Filip Gurak <filip.gurak999@gmail.com>
2023-05-29 14:15:32 +0200

    fixed responsivness

--- Components/ProductPageItem.tsx
+++ Components/ProductPageItem.tsx
@@ -25,7 +25,7 @@ function ProductPageItem({ product }: { product: Product }) {
   }
 
   return (
-    <div className='grid grid-cols-3 md:grid-cols-2 gap-12 lg:gap-20 '>
+    <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-2 sm:gap-12 lg:gap-20 '>
       <div className='flex items-center justify-center'>
         <Image
           src={product.image?.desktop!}
@@ -39,16 +39,23 @@ function ProductPageItem({ product }: { product: Product }) {
           alt={product.name}
           width={300}
           height={300}
-          className='col-span-1 md:hidden rounded-md'
+          className='col-span-1 hidden sm:block md:hidden rounded-md'
+        />
+        <Image
+          src={product.image?.mobile!}
+          alt={product.name}
+          width={1920}
+          height={1080}
+          className='sm:hidden rounded-md mb-6'
         />
       </div>
       <div className='flex flex-col col-span-2 md:col-span-1 justify-center'>
         {product.new && (
-          <p className='text-orange uppercase tracking-[0.4em] mb-2'>
+          <p className='text-orange text-sm md:text-base uppercase tracking-[0.4em] mb-2'>
             new product
           </p>
         )}
-        <h2 className='font-semibold text-4xl mb-4 uppercase'>
+        <h2 className='font-semibold text-2xl sm:text-4xl mb-4 uppercase'>
           {product.name}
         </h2>
         <p className='text-light-black mb-4 text-sm md:text-base'>
@@ -57,24 +64,26 @@ function ProductPageItem({ product }: { product: Product }) {
         <p className='font-bold mb-4'>
           $ {product.price.toLocaleString('en-US')}
         </p>
-        <div className='flex'>
-          <button
-            onClick={() => setQty((prevQty) => prevQty - 1)}
-            disabled={qty === 0}
-            className='bg-light-gray text-light-black  text-xl font-bold py-2 px-4 hover:bg-gray-200 hover:text-orange transition-all'
-          >
-            -
-          </button>
+        <div className='flex justify-between'>
+          <div className='flex'>
+            <button
+              onClick={() => setQty((prevQty) => prevQty - 1)}
+              disabled={qty === 0}
+              className='bg-light-gray text-light-black  text-xl font-bold py-2 px-4 hover:bg-gray-200 hover:text-orange transition-all'
+            >
+              -
+            </button>
 
-          <div className='bg-light-gray text-black flex items-center justify-center w-10 h-full'>
-            {qty}
+            <div className='bg-light-gray text-black flex items-center justify-center w-10 h-full'>
+              {qty}
+            </div>
+            <button
+              onClick={() => setQty((prevQty) => prevQty + 1)}
+              className='bg-light-gray text-light-black text-xl font-bold py-2 px-4 mr-2 hover:bg-gray-200 hover:text-orange transition-all'
+            >
+              +
+            </button>
           </div>
-          <button
-            onClick={() => setQty((prevQty) => prevQty + 1)}
-            className='bg-light-gray text-light-black text-xl font-bold py-2 px-4 mr-4 hover:bg-gray-200 hover:text-orange transition-all'
-          >
-            +
-          </button>
           <MainBtn onClick={addToCartHandler}>Add to cart</MainBtn>
         </div>
       </div>
